c6544be736663ddbd4b6a399eed520d75aa9dc30
shilpa kulkarni <shilpa.a.kulkarni@oracle.com>
2019-06-06 11:00:47 +0530

    server.js changes

--- server.js
+++ server.js
@@ -1,11 +1,9 @@
-var path = require('path');
 var express = require('express');
-
 var app = express();
 
-var staticPath = path.join(__dirname, '/app');
-app.use(express.static(staticPath));
+app.use(express.static('app'));
+app.use(express.static('images'));
 
-app.listen(3000, function() {
-  console.log('listening on port 3000');
-});
+console.log("test")
+var port = process.env.PORT || 3000;
+app.listen(port);
